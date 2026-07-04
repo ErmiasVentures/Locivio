4a3ce14baa778323d33ceff775953fb3781435b1
Ermias Tesgay <etesgayventures@gmail.com>
2026-07-04 10:08:26 -0400

    Added Firebase Configuration

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// fire.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9_ZYYfmh-vbUTpuKOzq_BQAmR7LZnkE",
+  authDomain: "locivio-febeb.firebaseapp.com",
+  projectId: "locivio-febeb",
+  storageBucket: "locivio-febeb.firebasestorage.app",
+  messagingSenderId: "1048529393580",
+  appId: "1:1048529393580:web:a5505378d9add537234c09",
+  measurementId: "G-1J3ZZ16N3M"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
